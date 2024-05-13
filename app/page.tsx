d6904f405b3fb20e5a9d3390890e51b571069f0f
shiro-web <shiro-kwsm-46@docomo.ne.jp>
2024-05-13 20:54:09 +0900

    useGetData消去

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-// import useGetData from './hooks/useGetData.ts';
 interface Row {
   id: string;
   nickname: string;
