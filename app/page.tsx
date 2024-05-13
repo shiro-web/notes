12f75011bbb39206c026208a720c7af6cfa124dd
shiro-web <shiro-kwsm-46@docomo.ne.jp>
2024-05-13 20:46:00 +0900

    型付け

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,21 @@ import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import useGetData from './hooks/useGetData.ts';
-
+interface Row {
+  id: string;
+  nickname: string;
+  likeCount: number;
+  eyecatch: string;
+  noteUrl: string;
+  articleName: string;
+}
 export default function Home() {
   const [rakutens, setRakutens] = useState(undefined);
   const [notes, setNotes] = useState([]);
   const [users,setUsers] = useState([])
   const [counts,setCounts] = useState<number>()
   const [pages,setPages] = useState<number>()
-  const [rows,setRows] = useState<string>()
+  const [rows,setRows] = useState<Row[]>()
   const [datas,setDatas] = useState<string>()
   const [tests,setTests] = useState<string>()
   // useGetData();
@@ -37,11 +44,11 @@ useEffect(() => {
             <div className="mb-6" key={row.id}>
               <div className="flex text-xl gap-2">
                 <p>{index + 1}位</p>
-                <p >{row.nickname}</p>
+                <p>{row.nickname}</p>
               </div>
               <p className="font-bold">スキ数：<span className=" text-pink-500">{row.likeCount}</span></p>
               <Link className="hover:opacity-50 duration-100" href={row.noteUrl}>
-                <Image src={row.eyecatch} width={500} height={500}/>
+                <Image src={row.eyecatch} width={500} height={500} alt="eyecatch"/>
                 <p className="font-bold">記事タイトル：{row.articleName}</p>
               </Link>
             </div>
